1c4d3dcc64c983b69539689d03f5b34e7891ba56
Alan Hernandez <alan23bh@gmail.com>
2025-12-05 07:38:51 -0500

    Fixed Auth sign in bug

--- src/app/auth/signin/SignIn.tsx
+++ src/app/auth/signin/SignIn.tsx
@@ -1,26 +1,53 @@
+// "use client";
+// import { useSession } from "next-auth/react";
+// import { Box } from "@mui/material";
+// import Login from "@/app/components/Login/Login";
+
+// export const SignIn = (p0: string, p1: { callbackUrl: string }) => {
+//   const { data: session } = useSession();
+
+//   return (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <h2>{session ? "Thank you for logging in" : ""}</h2>
+//       <Login />
+//     </Box>
+//   );
+// };
+
+// export default SignIn;
 "use client";
+
 import { useSession } from "next-auth/react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Login from "@/app/components/Login/Login";
 
-export const SignIn = (p0: string, p1: { callbackUrl: string }) => {
+export default function SignIn() {
   const { data: session } = useSession();
 
   return (
     <Box
       sx={{
+        minHeight: "calc(100vh - 128px)",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        textAlign: "center",
       }}
     >
-      <h2>{session ? "Thank you for logging in" : ""}</h2>
+      {session && (
+        <Typography variant="h6" sx={{ mb: 2 }}>
+          Thank you for logging in
+        </Typography>
+      )}
       <Login />
     </Box>
   );
-};
-
-// NEW
-
-export default SignIn;
+}
